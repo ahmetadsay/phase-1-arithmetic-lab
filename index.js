ebd20c3436a74f68e674be40d008dbb5e206e010
ahmet <adsayahmet94@hotmail.com>
2023-04-07 01:01:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14
+let num4 = 5
+const mod = 14 % 5;
+
+const max = Math.max(5, 10, 15, 20, 17);
